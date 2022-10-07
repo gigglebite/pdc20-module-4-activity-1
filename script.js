e9bebbb9d7976a4c8be43e1a658494bc43f27ea4
gigglebite <candelaria.aleeyajenzen@auf.edu.ph>
2022-10-07 16:14:30 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Just a simple function that will get the sum of a and b:
+function mathKuno(){
+    let a=10;
+    let b=5;
+    let $ ="Sum"
+    alert(`The ${$} of ${a} and ${b} is: ${a+b}.`);
+    }
